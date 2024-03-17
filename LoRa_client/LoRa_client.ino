de2c82351e5109bfcf66fc0a321b4177c8adec40
bobcat <noakes.bob@gmail.com>
2024-03-16 19:06:30 -0600

    Initial commit. LoRa Client and LoRa server are working as a proof of concept. The server looks for a LoRa message containing the requested servo position and commands the servo to that position. The Server also sends back an analog sensor value to the client, which the client reads.

---
+++ LoRa_client/LoRa_client.ino
@@ -0,0 +1,67 @@
+#include <SPI.h>
+#include <LoRa.h>
+#include <LiquidCrystal.h>
+
+// Global variables
+char msg;
+const int rs = 7, en = 6, d4 = 5, d5 = 4,  d6 = 3, d7 = 2;  // Set pins for LCD. Some are used by the LoRa board but not sure which?
+int sensorPin = A2;
+int potVoltage = 0;
+long transmitUpdateInterval = 500;  // Send a radio transmit via LoRa every # milliseconds
+unsigned long previousTransmit = millis();
+
+LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
+
+void setup() {
+  lcd.begin(16, 2);  // Start LCD for 16 x 2 grid
+  Serial.begin(9600);  // Start serial, 9600 baud rate
+  while (!Serial);
+
+  Serial.println("LoRa Receiver");
+
+  if (!LoRa.begin(915600000)) {  // Start LoRa, 915 MHz
+    Serial.println("Starting LoRa failed!");
+    while (1);
+  }
+
+  LoRa.setSyncWord(0x34);  // TODO: investigate this function
+  lcd.clear();
+}
+
+void loop() {
+  unsigned long currentTime = millis();  // Get the current time for "multithreading"
+  
+  // Get the controller pot readings
+  potVoltage = analogRead(sensorPin);
+  potVoltage = map(potVoltage, 0, 1023, 1000, 2000);     // scale it to use it with the servo (value between 800 and 2200 micro-seconds)
+
+  
+  // try to parse packet, if available
+  int packetSize = LoRa.parsePacket();  // TODO: investigate this function
+  if (packetSize) {
+    // received a packet
+    lcd.setCursor(0, 1);
+
+    // read packet
+    while (LoRa.available()) {
+      msg = LoRa.read();
+      Serial.print(msg);
+      lcd.print(msg);
+    }
+  }
+   // Transmit a packet if the time interval has elapsed.
+  if (currentTime - previousTransmit > transmitUpdateInterval)  {
+    controller_send_packet();
+    }  
+}
+
+
+// Send a data packet
+void controller_send_packet() {
+    // Send outgoing packet
+  Serial.println(potVoltage);
+  LoRa.beginPacket();
+  LoRa.print(potVoltage);
+  LoRa.endPacket();
+  previousTransmit = millis();
+  }
---
+++ LoRa_server/LoRa_server.ino
@@ -0,0 +1,86 @@
+#include <Servo.h>
+#include <SPI.h>
+#include <LoRa.h>
+
+// Global variables
+int trimPin = A0;  //  Pin to take in analog position/potentiometer voltage
+int sailTrim = 0;
+int servoPosition = 1520;  // Initialize rudder position to neutral
+long servoUpdateInterval = 30;  // Servo updates every 15 ms (smooth-ish)
+unsigned long previousTimeServo = millis();
+long radioUpdateInterval = 500;  // Interval for updating LoRa radio communication;
+unsigned long previousTimeRadio = millis();
+
+Servo rudderServo;
+
+void setup() {
+  Serial.begin(9600);
+  while (!Serial);  // Wait unitl serial starts
+
+  if (!LoRa.begin(915600000)) {
+    Serial.println("Starting LoRa failed!");
+    while (1);
+  }
+  LoRa.setSyncWord(0x34);  // TODO: what's this function for?
+  rudderServo.attach(5);  // Attaches servo to pin (6) on the servo object
+}
+
+void loop() {
+  unsigned long currentTime = millis();  // Take current time for "multithreading"
+
+  receive_message();  // Receive a message over LoRa
+
+  // Update servo position if the update interval has elapsed
+  if (currentTime - previousTimeServo > servoUpdateInterval) {
+
+    // Servo control
+    rudderServo.writeMicroseconds(servoPosition);
+
+    // Update previous update time to current time
+    previousTimeServo = millis();
+  }
+
+  // Send new packet if the radio update interval has elapsed
+  if (currentTime - previousTimeRadio > radioUpdateInterval) {
+    send_message();
+  }
+}
+
+
+void receive_message() {
+  // Receive packet via LoRa client (remote control)
+  int packetSize = LoRa.parsePacket();  // TODO: investigate this function
+  char msg[packetSize];  // Create a character array equal to the size of the incoming packet
+  if (packetSize) {
+    Serial.println("Received packet");
+    int i = 0;
+
+    // Read packet
+    while (LoRa.available()) {
+      char character = LoRa.read();
+
+      msg[i] = character;
+      i++;
+    }
+    const char *ptr_msg = msg;
+    servoPosition = atoi(ptr_msg);
+    Serial.println(msg);
+    Serial.println("packet complete");  
+    // servoPosition = atoi(msg);
+    Serial.println(servoPosition);
+  }
+}
+
+
+void send_message() {
+    sailTrim = analogRead(trimPin);  // Get the sail trim angle
+    Serial.println("Sending packet: ");  // TODO: delete once debugging complete
+    Serial.println(sailTrim);
+
+    // send packet
+    LoRa.beginPacket();
+    LoRa.print("voltage: ");
+    LoRa.print(sailTrim);
+    LoRa.endPacket();  // TODO: Does this add a header or something?
+    previousTimeRadio = millis();  // Update previous time for radio send
+  }
