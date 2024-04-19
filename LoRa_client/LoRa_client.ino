0542a12e90376766db582c3752f3be30e6b77f12
bobcatCA <noakes.bob@gmail.com>
2024-04-19 12:52:47 -0700

    Fixed LoRa transmission so that only bytes are transmitted, vs Strings/chars. Had a hardware bug with an analog pin connected to an OFF Arduino. Working well now, next challenge is to improve the latency, standardize #define vs int assignments, and general cleanup/housekeeping.

--- LoRa_client/LoRa_client.ino
+++ LoRa_client/LoRa_client.ino
@@ -11,18 +11,15 @@ int sailVoltage = 0;
 const int csPin = 7;          // LoRa radio chip select
 const int irqPin = 1;         // change for your board; must be a hardware interrupt pin
 const int resetPin = 6;       // LoRa radio reset
-int rudderControlPin = A2;
-int sailControlPin = A3;
+int rudderControlPin = A0;
+int sailControlPin = A2;
 
 // Globals for messages incoming/outging via LoRa
-byte msgCount = 0;            // count of outgoing messages
 byte localAddress = 0xBB;     // address of this device
 byte destination = 0xFF;      // destination to send to
 int sendInterval = 200;          // interval between sends
+int sailTrim;
 long lastSendTime = 0;        // last send time
-String incoming;              // incoming message
-String lcdStatus;
-String outgoing;              // outgoing message
 
 // Start LCD display
 LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
@@ -45,67 +42,65 @@ void setup() {
 
 
 void loop() {
+  // Cleaer LCD and get an incoming data packet from LoRa. TODO: fix onReceive return value.
   lcd.clear();
-  incoming = onReceive(LoRa.parsePacket());
-  if (incoming.length() == 11)  {
-    Serial.println("Message: " + incoming);
-    lcdStatus = incoming;  
-    }
-    
+  String incoming = onReceive(LoRa.parsePacket());
+
+  // Get sensor readings for controller pots
   rudderVoltage = analogRead(rudderControlPin);  // Get the sensor voltage and convert to 10-bit value
-  rudderVoltage = map(rudderVoltage, 0, 1023, 1000, 2000);  // scale it to use it with the servo (value between 800 and 2200 micro-seconds)
+  int rudderCommandDegrees = map(rudderVoltage, 0, 1023, 0, 270);  // scale it to use it with the servo (value between 800 and 2200 micro-seconds)
   sailVoltage = analogRead(sailControlPin);  // Get the sensor voltage and convert to 10-bit value
-  sailVoltage = map(sailVoltage, 0, 1023, 1000, 2000);
+  int sailCommandDegrees = map(sailVoltage, 0, 1023, -90, 90);
+  int sailTrimDegrees = map(sailTrim, 0, 1023, -90, 90);
 
   // Display controller values on lcd screen
   lcd.setCursor(0, 0);
-  lcd.print(rudderVoltage);
+  lcd.print(rudderCommandDegrees);
   lcd.setCursor(0, 1);
-  lcd.print(sailVoltage);    
+  lcd.print(sailCommandDegrees);
   lcd.setCursor(5, 1);
-  lcd.print(lcdStatus);
+  lcd.print(sailTrimDegrees);
   delay(10);
 
   if (millis() - lastSendTime > sendInterval) {
-    String message = "ruddercommand" + String(rudderVoltage) + "sailcommand" + String(sailVoltage);
-    sendMessage(message);
-    Serial.println("Sending " + message);
+    String message = "command";
+    sendMessage(rudderVoltage, sailVoltage, message);
     lastSendTime = millis();            // timestamp the message
     sendInterval = random(sendInterval) + 100;    // 200-300 milliseconds
-    }
+  }
 }
 
 
-void sendMessage(String outgoing) {
+// Function for sending packet.
+void sendMessage(int sensorReading1, int sensorReading2, String outgoing) {
   LoRa.beginPacket();                   // start packet
   LoRa.write(destination);              // add destination address
   LoRa.write(localAddress);             // add sender address
-  LoRa.write(msgCount);                 // add message ID
-  LoRa.write(outgoing.length());        // add payload length
-  LoRa.print(outgoing);                 // add payload
+  //LoRa.write(msgCount);                 // add message ID
+  //LoRa.write(outgoing.length());        // add payload length
+  LoRa.write(sensorReading1 & 255);  // Pad so the message is always 16-bit length
+  LoRa.write((sensorReading1 >> 8) & 0xff);  // Shift 8 bits
+  LoRa.write(sensorReading2 & 255);  // Pad so the message is always 16-bit length
+  LoRa.write((sensorReading2 >> 8) & 0xff);  // Shift 8 bits
+  //LoRa.print(outgoing);                 // add payload
   LoRa.endPacket();                     // finish packet and send it
-  msgCount++;                           // increment message ID
+  //msgCount++;                           // increment message ID
 }
 
 String onReceive(int packetSize) {
   if (packetSize == 0) return "NO PACKET";  // if there's no packet, return
 
+
   // read packet header bytes:
   int recipient = LoRa.read();          // recipient address
   byte sender = LoRa.read();            // sender address
-  byte incomingMsgId = LoRa.read();     // incoming msg ID
-  byte incomingLength = LoRa.read();    // incoming msg length
+  //byte incomingMsgId = LoRa.read();     // incoming msg ID
+  //byte incomingLength = LoRa.read();    // incoming msg length
+  int sensorReading = LoRa.read();
+  sensorReading = LoRa.read()<<8 | sensorReading;
+  sailTrim = sensorReading;
 
   String incomingMessage = "";
-
-  while (LoRa.available()) {
-    incomingMessage += (char)LoRa.read();
-  }
-
-  if (incomingLength != incomingMessage.length()) {   // check length for error
-    Serial.println("error: message length does not match length");
-    return "ERROR";                     // skip rest of function
-  }
 
   // if the recipient isn't this device or broadcast,
   if (recipient != localAddress && recipient != 0xFF) {
@@ -114,13 +109,12 @@ String onReceive(int packetSize) {
   }
 
   // if message is for this device, or broadcast, print details:
-  /*Serial.println("Received from: 0x" + String(sender, HEX));
+  /*
+  Serial.println("Received from: 0x" + String(sender, HEX));
   Serial.println("Sent to: 0x" + String(recipient, HEX));
-  Serial.println("Message ID: " + String(incomingMsgId));
-  Serial.println("Message length: " + String(incomingLength));
-  Serial.println("Message: " + incomingMessage);
   Serial.println("RSSI: " + String(LoRa.packetRssi()));
   Serial.println("Snr: " + String(LoRa.packetSnr()));
-  Serial.println();*/
+  Serial.println();
+  */
   return incomingMessage;
 }
--- LoRa_server/LoRa_server.ino
+++ LoRa_server/LoRa_server.ino
@@ -8,9 +8,8 @@
 
 // Global variables for command/control
 int sailTrim = 0;
-int rudderCommand;  // incoming command for rudder position
-int rudderTest = 858;  // TODO: delete once the bitwise wire send is working
-int sailCommand;  // incoming command for sail position
+int rudderTrimCommand;  // incoming command for rudder position
+int sailTrimCommand;  // incoming command for sail position
 int servoPosition;  // Initialize rudder position to neutral
 long servoUpdateInterval = 15;  // Servo updates every 15 ms (smooth-ish)
 unsigned long previousTimeServo = millis();
@@ -22,13 +21,10 @@ const int irqPin = 1;         // change for your board; must be a hardware inter
 int trimPin = A0;  //  Pin to take in analog position/potentiometer voltage
 
 // Globals for message incoming/outgoing via LoRa
-byte msgCount = 0;            // count of outgoing messages
 byte localAddress = 0xBB;     // address of this device
 byte destination = 0xFF;      // destination to send to
 int sendInterval = 2000;          // interval between sends
 long lastSendTime = 0;        // last send time
-String incoming;              // incoming message
-String outgoing;              // outgoing message
 
 // Special Globals
 Servo rudderServo;
@@ -48,15 +44,7 @@ void setup() {
 }
 
 void loop() {
-  int expectedPacketLength = 32;  // Number of characters of normal incoming message
-  incoming = onReceive(LoRa.parsePacket());
-
-  // Parse the incoming packet. If the length is correct, parse the various commands from the incoming packet
-  if (incoming.length() == expectedPacketLength)  {
-    rudderCommand = incoming.substring(13, 17).toInt();
-    sailCommand = incoming.substring(28, 33).toInt();
-    Serial.println(incoming);
-  }
+  String incoming = onReceive(LoRa.parsePacket());  // TODO: Fix what returns from onReceive
 
   // Read the sensor values to be sent back to the controller node
   sailTrim = analogRead(trimPin);
@@ -65,8 +53,8 @@ void loop() {
   if (millis() - previousTimeServo > servoUpdateInterval) {
 
     // Servo control only if the setpoint is well off of the real position
-    if (abs(rudderCommand - servoPosition) > 5)  {
-      servoPosition = servoPosition + (rudderCommand - servoPosition) / 5;  // Increment slowly until the position responds (P-control of sorts)
+    if (abs(rudderTrimCommand - servoPosition) > 5)  {
+      servoPosition = servoPosition + (rudderTrimCommand - servoPosition) / 5;  // Increment slowly until the position responds (P-control of sorts)
       rudderServo.writeMicroseconds(servoPosition);
     }
 
@@ -77,7 +65,7 @@ void loop() {
   // Send an outgoing packet to the controller note if enough time has elapsed.
   if (millis() - lastSendTime > sendInterval) {
     String message = "sailtrim" + String(sailTrim);   // send a message
-    sendMessageLoRa(message);
+    sendMessageLoRa(sailTrim, message);
     Serial.println("Sending " + message);
     lastSendTime = millis();            // timestamp the message
     sendInterval = random(sendInterval) + 1000;    // 2-3 seconds
@@ -86,61 +74,58 @@ void loop() {
     Wire.beginTransmission(4);  // Send to address 4 (stepper server)
     Wire.write(sailTrim >> 8);  // Shift 8 bits
     Wire.write(sailTrim & 255);  // Pad so the message is always 16-bit length
-    Wire.write(sailCommand >> 8);  // Shift 8 bits
-    Wire.write(sailCommand & 255);
+
+    Wire.write(sailTrimCommand >> 8);  // Shift 8 bits
+    Wire.write(sailTrimCommand & 255);  // Pad so the message is always 16-bit length
     Wire.endTransmission();
   }
 }
 
 
-// Function for receiving packet.
+// Function for sending packet.
+void sendMessageLoRa(int sensorReading, String outgoing) {
+  LoRa.beginPacket();                   // start packet
+  LoRa.write(destination);              // add destination address
+  LoRa.write(localAddress);             // add sender address
+  //LoRa.write(msgCount);                 // add message ID
+  //LoRa.write(outgoing.length());        // add payload length
+  LoRa.write(sensorReading & 255);  // Pad so the message is always 16-bit length
+  LoRa.write((sensorReading >> 8) & 0xff);  // Shift 8 bits
+  LoRa.print(outgoing);                 // add payload
+  LoRa.endPacket();                     // finish packet and send it
+  //msgCount++;                           // increment message ID
+}
+
+
 String onReceive(int packetSize) {
   if (packetSize == 0) return "NO PACKET";  // if there's no packet, return
 
   // read packet header bytes:
   int recipient = LoRa.read();          // recipient address
   byte sender = LoRa.read();            // sender address
-  byte incomingMsgId = LoRa.read();     // incoming msg ID
-  byte incomingLength = LoRa.read();    // incoming msg length
-
+  int rudderSensor = LoRa.read();
+  rudderSensor = LoRa.read()<<8 | rudderSensor;
+  rudderTrimCommand = rudderSensor;
+  rudderTrimCommand = map(rudderTrimCommand, 0, 1023, 900, 2100);  // re-scale to between 900 and 2100 (for servo input)
+  int sailSensor = LoRa.read();
+  sailSensor = LoRa.read()<<8 | sailSensor;
+  sailTrimCommand = sailSensor;
+  
   String incomingMessage = "";
 
-  while (LoRa.available()) {
-    incomingMessage += (char)LoRa.read();
-  }
-
-  if (incomingLength != incomingMessage.length()) {   // check length for error
-    Serial.println("error: message length does not match length");
-    return "ERROR";                     // skip rest of function
-  }
-
-  // if the recipient isn't this device or broadcast,
+    // if the recipient isn't this device or broadcast,
   if (recipient != localAddress && recipient != 0xFF) {
     Serial.println("This message is not for me.");
     return "ERROR";                     // skip rest of function
   }
 
   // if message is for this device, or broadcast, print details:
-  /*Serial.println("Received from: 0x" + String(sender, HEX));
-    Serial.println("Sent to: 0x" + String(recipient, HEX));
-    Serial.println("Message ID: " + String(incomingMsgId));
-    Serial.println("Message length: " + String(incomingLength));
-    Serial.println("Message: " + incomingMessage);
-    Serial.println("RSSI: " + String(LoRa.packetRssi()));
-    Serial.println("Snr: " + String(LoRa.packetSnr()));
-    Serial.println();*/
+  /*
+  Serial.println("Received from: 0x" + String(sender, HEX));
+  Serial.println("Sent to: 0x" + String(recipient, HEX));
+  Serial.println("RSSI: " + String(LoRa.packetRssi()));
+  Serial.println("Snr: " + String(LoRa.packetSnr()));
+  Serial.println();
+  */
   return incomingMessage;
-}
-
-
-// Function for sending packet.
-void sendMessageLoRa(String outgoing) {
-  LoRa.beginPacket();                   // start packet
-  LoRa.write(destination);              // add destination address
-  LoRa.write(localAddress);             // add sender address
-  LoRa.write(msgCount);                 // add message ID
-  LoRa.write(outgoing.length());        // add payload length
-  LoRa.print(outgoing);                 // add payload
-  LoRa.endPacket();                     // finish packet and send it
-  msgCount++;                           // increment message ID
 }
