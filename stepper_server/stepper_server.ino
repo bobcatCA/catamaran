37a857dcd1fa15a2973cf7971755875e7a661520
bobcat <noakes.bob@gmail.com>
2024-08-06 21:36:26 -0600

    Modified the stop-stepper conditions to have proper booleans. Added Circular Buffer global variable to track the last 10 errors, used for sheet-slack condition detection. Something is wrong with the logic as it is not entering the stop-function. Math is working but some logic flaw, next to investigate.

--- stepper_server/stepper_server.ino
+++ stepper_server/stepper_server.ino
@@ -4,6 +4,7 @@
 
 // Librairies
 #include <AccelStepper.h>
+#include <CircularBuffer.hpp>
 #include <PID_v1.h>
 #include <SPI.h>
 #include <Wire.h>
@@ -13,17 +14,25 @@
 #define  enPin  7
 #define  stepPin  2
 
-// Define our maximum and minimum speed in steps per second, mid-zone range
+// Define our maximum and minimum speed in steps per second
+#define  errorThreshold -20
 #define  maximumSpeed 10000
 #define  minimumSpeed 0
-#define midZone 7
+#define  pauseTimeCheck 500
+#define  trimBand 5
+#define  trimCenter 5
 
 // Globals initialization
+bool onTarget = false;
+bool onMiddle = true;
+bool sheetSlack = true;
+CircularBuffer<int, 10> posErrors;
+int currentSpeed = 0;
+int errorsAverage = 0;
 double setpoint, input, output;  // PID variables
-double Kp = 3, Ki = 0, Kd = 0;  // PID constants (to be tuned)
-int operatingBand = 5;  // Margin within which the motor is inactive, to save power consumption
-unsigned long pidUpdateInterval = 800;  // Frequency of analog reads, in milliseconds
-unsigned long previousTimePid;  // Tracks when the last analog read occured
+double Kp = 1, Ki = 0, Kd = 0;  // PID constants (to be tuned)
+unsigned long inputUpdateInterval = 80;  // Frequency of analog reads, in milliseconds
+unsigned long previousTimeAnalog;  // Tracks when the last analog read occured
 
 // Declare Stepper and PID
 AccelStepper stepper1(AccelStepper::DRIVER, stepPin, dirPin);
@@ -41,7 +50,7 @@ void setup() {
   stepper1.enableOutputs();  // Set enable to TRUE (disabled if input close to setpoint)
 
   // Initialize linked variables
-  previousTimePid = millis();
+  previousTimeAnalog = millis();
   sailPID.SetMode(AUTOMATIC);
   sailPID.SetOutputLimits(-255, 255);  // Bi-directional for motor CW/CCW
 }
@@ -49,27 +58,49 @@ void setup() {
 void loop() {
 
   // Update analog readings if the interval of time has passed
-  if (millis() - previousTimePid > pidUpdateInterval)  {
+  if (millis() - previousTimeAnalog > inputUpdateInterval)  {
 
     // Compute new PID output given the new setpoint/input. scale the speed to appropriate limits.
     sailPID.Compute();
-    int current_speed = ((output / 255) * (maximumSpeed - minimumSpeed)) + minimumSpeed;
-    stepper1.setSpeed(current_speed);
-    previousTimePid = millis();  // Update the last time analogs were read
+    int errorPID = input - setpoint;  // Compute error for this step
+    posErrors.push(errorPID);  // Push the error on this step to the top of the buffer
+    errorsAverage = arrayAverage();
 
+    // Compute the new speed setpoint and send to stepper
+    currentSpeed = ((output / 255) * (maximumSpeed - minimumSpeed)) + minimumSpeed;
+    previousTimeAnalog = millis();  // Update the last time analogs were read
+
+    // Compute booleans
+    if (abs(setpoint - input) < trimBand)  {
+      onTarget = true;
+    } else {
+      onTarget = false;
+    }
+
+    if (currentSpeed < 0 && abs(input) < trimCenter)  {
+      onMiddle = true;
+    } else {
+      onMiddle = false;
+    }
+
+    if (setpoint > input && errorsAverage < errorThreshold)  {
+      sheetSlack = true;
+    } else {
+      sheetSlack = false;
+    }
+    
   }
-  
+
+  Serial.println(errorsAverage);
   // While the input and setpoint are close, shut down the motor and wait until they diverge again
-  if (abs(setpoint - input) < operatingBand)  {
-    pauseOnTarget();
+  if (onTarget || onMiddle || sheetSlack)  {
+    pauseSailControl();
   }
+  stepper1.enableOutputs();
+  stepper1.setSpeed(currentSpeed);
 
-  // Avoid running stepper tighter if the boom is already near the center to avoid damage.
-  if (!(setpoint < input && input < midZone))  {
-    stepper1.runSpeed();
-  } else {
-    pauseOnMiddle();
-  }
+  // Run the stepper at the desired speed
+  stepper1.runSpeed();
 }
 
 
@@ -82,32 +113,31 @@ void receiveI2C(int numBytes)  {
   input = sailTrim;
   stepper1.runSpeed();  // Additional call for smoothness
 
-  // Bit shift to get input sail command
+  // Bit shift to get input rudder command
   int sailCommand  = Wire.read() << 8;
   stepper1.runSpeed();  // Additional call for smoothness
   sailCommand |= Wire.read();
   setpoint = sailCommand;
-  stepper1.runSpeed();  // Additional call for smoothness
-}
-
-
-// Function to hold in place if the input and setpoint are close
-void pauseOnTarget()  {
-  stepper1.disableOutputs();  // Stop the stepper motor when close to the target/setpoint
-  while (abs(setpoint - input) < operatingBand)  {
-    delay(10);
-    // Loiter here
-  }
-  stepper1.enableOutputs();
 }
 
 
 // Function to hold in place if the sail position is in the middle (prevent over-winding)
-void pauseOnMiddle()  {
+void pauseSailControl()  {
   stepper1.disableOutputs();  // Stop the stepper motor when close to the target/setpoint
-  while ((setpoint < input && input < midZone))  {
-    delay(10);
-    // Loiter here
+  int startTime = millis();
+  while (millis() - startTime < pauseTimeCheck)  {
+    delay(100);
+    // Loiter here, then go back and check analogs
   }
-  stepper1.enableOutputs();
+}
+
+
+int arrayAverage()  {
+  int arrLength = 10 - posErrors.available();
+  int sum = 0;
+  for (int i = 0; i < arrLength; i++)  {
+    sum = sum + posErrors[i];
+  }
+  int average = sum / 10;
+  return average;
 }
